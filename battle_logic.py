e82e04ec471a4b8c4ba399e1fba3f16d85816572
LizzieDworsky <amydworsky15@gmail.com>
2022-08-30 13:50:57 -0600

    added battle logic between hero and enemy, WIP

--- battle_logic.py
+++ battle_logic.py
@@ -1,4 +1,5 @@
 import characters
+import random
 
 """
 The "Run Game" function
@@ -11,7 +12,27 @@ Print victory message
 
 def run_game():
     welcome()
+    battle_phase(characters.hero_goku, characters.enemy_frieza)
+    # battle_phase(characters.hero_goku, characters.enemy_cell)
+    # battle_phase(characters.hero_goku, characters.enemy_kid_buu)
 
 def welcome():
     print("Welcome to DragonBall RPG")
+    print(f"{characters.hero_goku['name']} is the hero!")
+    print("He needs to defeat")
+    print(characters.enemy_frieza['name'])
+    print(characters.enemy_cell['name'])
+    print(f"and {characters.enemy_kid_buu['name']}!")
 
+def battle_phase(hero, enemy):
+    print(f"{hero['name']} enters into battle with {enemy['name']}")
+    while hero['health'] > 0 and enemy['health'] > 0:
+        random_attack = random.choice(hero['attacks'])
+        print(f"{hero['name']} attacks {enemy['name']} using {random_attack[0]} for {random_attack[1]} damage")
+        enemy['health'] -= random_attack[1]
+        print(f"{enemy['name']} now has {enemy['health']} health.")
+        if enemy['health'] > 0:
+            random_attack = random.choice(enemy['attacks'])
+            print(f"{enemy['name']} attacks {hero['name']} using {random_attack[0]} for {random_attack[1]} damage")
+            hero['health'] -= random_attack[1]
+            print(f"{hero['name']} now has {hero['health']} health.")
